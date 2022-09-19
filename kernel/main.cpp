f6e7dc7b01a8705d483fd50bced8b91abdc02a42
Dangornushi <dangomushi@Yuma.local>
2022-09-19 19:01:46 +0900

    BUG: ExitBootServicesとかいう害悪のせいで一向に進みません

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,11 +1,36 @@
 #include <cstdint>
+#include "frame_buffer_config.hpp"
 
-extern "C" void KernelMain(uint64_t frame_buffer_base,
-                           uint64_t frame_buffer_size) {
-  uint8_t *frame_buffer = reinterpret_cast<uint8_t *>(frame_buffer_base);
-  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
-    frame_buffer[i] = 23;
-  }
-  while (1)
-    __asm__("hlt");
+struct PixelColor {
+    uint8_t r, g, b;
+};
+
+int WritePixel(const FrameBufferConfig &config, int x, int y, const PixelColor &c) {
+    const int pixelPosition = config.pixelsPerScanLine * y + x;
+
+    if (config.pixelFormat == kPixelRGBResv8BitPerColor) {
+        uint8_t *p = &config.frameBuffer[4 * pixelPosition];
+        p[0]       = c.r;
+        p[1]       = c.g;
+        p[2]       = c.b;
+        return 0;
+    } else if (config.pixelFormat == kPixelBGRResv8BitPerColor) {
+        uint8_t *p = &config.frameBuffer[4 * pixelPosition];
+        p[0]       = c.b;
+        p[1]       = c.g;
+        p[2]       = c.r;
+        return 0;
+    }
+    return -1;
+}
+
+extern "C" void KernelMain(const FrameBufferConfig &frameBufferConfig) {
+    for (int x = 0; x < frameBufferConfig.horizontalResolution; ++x) {
+        for (int y = 0; y < frameBufferConfig.verticalResolution; ++y) {
+            WritePixel(frameBufferConfig, x, y, {0, 255, 0});
+        }
+    }
+
+    while (1)
+        __asm__("hlt");
 }
--- kernel/make.sh
+++ kernel/make.sh
@@ -4,8 +4,7 @@ source ~/osbook/devenv/buildenv.sh
 
 cd ~/workspace/mikanos/kernel
 
-clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp
-ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
+make
 
 cd ~/edk2
 
